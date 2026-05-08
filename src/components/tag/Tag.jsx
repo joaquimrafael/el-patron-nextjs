ae207c4ddd923f88ec9125b3c1be3b380812c453
ggarabs <ggarabetti0607@gmail.com>
2026-05-08 20:11:04 -0300

    add tag component module CSS

--- src/components/tag/Tag.jsx
+++ src/components/tag/Tag.jsx
@@ -1,9 +1,11 @@
+import styles from "@/components/tag/tag.module.css";
+
 export default function Tag({ name }) {
   const body = (
-    <button type="button" className="tag-button">
+    <button type="button" className={styles.tagButton}>
       {name}
     </button>
   );
 
-  return <li className="tag">{body}</li>;
+  return <li className={styles.tag}>{body}</li>;
 }
